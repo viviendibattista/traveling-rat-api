d3324ef21bdb28a3bd80ad714de03887d1a34bc8
vivien <vdibattista@corp.free.fr>
2023-08-14 16:30:29 +0200

    feat(Tips): create model and controller

---
+++ app/Http/Controllers/Api/TipController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Tip;
+use Illuminate\Http\Request;
+
+class TipController extends Controller
+{
+	public function index()
+	{
+		$tips = Tip::all();
+		return response()->json($tips);
+	}
+
+	public function show($id)
+	{
+		$tip = Tip::findOrFail($id);
+		return response()->json($post);
+	}
+}
